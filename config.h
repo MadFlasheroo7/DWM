8ff25833f26badb809f17d6c74faa1c0cf8ebc2e
MadFlasheroo7 <jayeshseth45@gmail.com>
2021-05-24 23:35:45 +0530

    added Dwm bar

--- config.h
+++ config.h
@@ -99,6 +99,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,		XK_m,	   spawn,	   SHCMD("java -jar /home/Mad_Flasher/Downloads/TLauncher-2.75.jar") },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
